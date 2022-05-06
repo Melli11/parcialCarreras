f19df703cf666d3dd28e20207f46cd74298736e1
GEREZ Martin <martin.gerez@kantaribopemedia.com>
2022-05-05 23:09:13 -0300

    Modelando autos

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,22 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+data Auto = Auto {
+    color :: Color,
+    velocidad :: Number,
+    distancia :: Number
+}deriving (Show,Eq)
+
+-- Carrera (Posicion,)
+
+data Color = Rojo | Verde | Amarillo | Azul deriving(Show,Eq)
+
+
+laDistanciaEsMenorA10 :: Auto -> Auto -> Bool
+laDistanciaEsMenorA10 unAuto deOtroAuto = (distancia unAuto - distancia deOtroAuto) <10
+
+losAutosSonDiferentes :: Auto -> Auto -> Bool
+losAutosSonDiferentes unAuto deOtroAuto = color unAuto /= color deOtroAuto
+
+estaCerca :: Auto -> Auto -> Bool
+estaCercaunAuto unAuto deOtroAuto = laDistanciaEsMenorA10 unAuto deOtroAuto && losAutosSonDiferentes unAuto deOtroAuto
